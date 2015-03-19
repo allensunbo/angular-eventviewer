36f0495d87fa7034d0c0dde37276c8348301249f
allensunbo <allensunbo@gmail.com>
2015-03-19 19:53:54 -0400

    more rowas

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -35,6 +35,29 @@ angular.module('angularEventviewerApp')
             severity: 'info'
         }];
 
+        var num = 1000;
+        for (var i = 0; i < num; i++) {
+            logs.push({
+                content: 'HTML5 Boilerplate',
+                createdBy: 'user-1',
+                severity: 'debug'
+            });
+        }
+        for (var i = 0; i < num; i++) {
+            logs.push({
+                content: 'HTML5 Boilerplate',
+                createdBy: 'user-2',
+                severity: 'info'
+            });
+        }
+        for (var i = 0; i < num; i++) {
+            logs.push({
+                content: 'HTML5 Boilerplate',
+                createdBy: 'user-3',
+                severity: 'warn'
+            });
+        }
+
         $scope.options = {
             data: logs,
             groupBy: 'createdBy',
